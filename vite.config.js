974d6819951f9920f75a1abb37eb6b72afa3351e
nagimasen <nagimasensushi@gamil.com>
2025-11-06 08:23:24 +0800

    fix base + rebuild dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/nagimasen-survey/',   // ← add this line (repo name exactly)
-});
+  base: '/nagimasen-survey/',
+})
